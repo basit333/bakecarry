013837f0239c1efe264344b6c1826980d08e971e
basit33 <shahbasit741@gmail.com>
2023-02-25 23:11:18 +0500

    added loader for page on load

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let mainContent = document.querySelector(".main-box");
+let loader = document.querySelector(".loader");
+
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+  mainContent.style.display = "block";
+});
